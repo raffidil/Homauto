084d81d2f37e03083ce52757ac72346eb2bc2cf9
Samim Pezeshki <p.samim@gmail.com>
2017-10-01 13:32:19 +0330

    Groups

--- db.js
+++ db.js
@@ -11,8 +11,28 @@ export const getDevices = () => {
     .catch(console.warn);
 };
 
-export const saveToDatabase = ({ devices }) => {
-  AsyncStorage.setItem('@MySuperStore:devices', JSON.stringify(devices)).catch(
-    console.warn
-  );
+export const getGroups = () => {
+  return AsyncStorage.getItem('@MySuperStore:groups')
+    .then(jsonString => {
+      const groups = JSON.parse(jsonString);
+      if (groups) return groups;
+
+      return [];
+    })
+    .catch(console.warn);
+};
+
+export const saveToDatabase = ({ devices, groups }) => {
+  if (devices) {
+    AsyncStorage.setItem(
+      '@MySuperStore:devices',
+      JSON.stringify(devices)
+    ).catch(console.warn);
+  }
+
+  if (groups) {
+    AsyncStorage.setItem('@MySuperStore:groups', JSON.stringify(groups)).catch(
+      console.warn
+    );
+  }
 };
